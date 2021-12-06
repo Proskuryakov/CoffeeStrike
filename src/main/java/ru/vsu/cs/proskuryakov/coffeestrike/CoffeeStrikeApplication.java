91c08cbeb668383075171eb4d6972b4d902aaf66
eproskuryakov <proskur.egor@yandex.ru>
2021-12-06 16:21:31 +0300

    initial commit

---
+++ src/main/java/ru/vsu/cs/proskuryakov/coffeestrike/CoffeeStrikeApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.proskuryakov.coffeestrike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeStrikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeStrikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vsu/cs/proskuryakov/coffeestrike/CoffeeStrikeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.proskuryakov.coffeestrike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeStrikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
