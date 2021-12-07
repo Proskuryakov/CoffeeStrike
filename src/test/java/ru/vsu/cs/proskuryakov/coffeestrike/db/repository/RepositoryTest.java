8bfcaf30196bc2026e70c219b81b8b0018af0cf7
eproskuryakov <proskur.egor@yandex.ru>
2021-12-07 12:48:15 +0300

    add repository template test

---
+++ src/test/java/ru/vsu/cs/proskuryakov/coffeestrike/db/repository/RepositoryTest.java
@@ -0,0 +1,72 @@
+package ru.vsu.cs.proskuryakov.coffeestrike.db.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RepositoryTest {
+
+    @Test
+    @DisplayName("Create category test")
+    void createCategoryTest() { }
+
+    @Test
+    @DisplayName("Get all category test")
+    void getCategoriesTest() { }
+
+    @Test
+    @DisplayName("Get category by id test")
+    void getCategoryByIdTest() { }
+
+    @Test
+    @DisplayName("Update category by id test")
+    void updateCategoryByIdTest() { }
+
+    @Test
+    @DisplayName("Delete category by id test")
+    void deleteCategoryByIdTest() { }
+
+    @Test
+    @DisplayName("Search category by name test")
+    void searchCategoryByNameTest() { }
+
+    @Test
+    @DisplayName("Create drink test")
+    void createDrinkTest() { }
+
+    @Test
+    @DisplayName("Get all drink test")
+    void getDrinksTest() { }
+
+    @Test
+    @DisplayName("Get drinks in category test")
+    void getDrinksByCategoryTest() { }
+
+    @Test
+    @DisplayName("Search drink by name in category test")
+    void searchDrinksByNameInCategoryTest() { }
+
+    @Test
+    @DisplayName("Get drink by id test")
+    void getDrinkByIdTest() { }
+
+    @Test
+    @DisplayName("Update drink by id test")
+    void updateDrinkByIdTest() { }
+
+    @Test
+    @DisplayName("Delete drink by id test")
+    void deleteDrinkByIdTest() { }
+
+    @Test
+    @DisplayName("Create product test")
+    void createProductTest() { }
+
+    @Test
+    @DisplayName("Get all product test")
+    void getProductsTest() { }
+
+    @Test
+    @DisplayName("Get all units test")
+    void getUnitsTest() { }
+
+}
